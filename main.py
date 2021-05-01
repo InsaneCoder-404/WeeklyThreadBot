b7dc1f49352e241820cc378effe569b9cdb978a6
InsaneCoder-404 <kemdavid11@gmail.com>
2021-05-01 15:57:08 +0800

    Initial Commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 
 reddit = praw.Reddit(client_id=os.environ.get('client_id'),
                      client_secret=os.environ.get('client_secret'),
-                     user_agent= 'Weekly Threads by /u/EmperorInkei v1.0',
+                     user_agent= os.environ.get('user_agent'),
                      username=os.environ.get('username'),
                      password=os.environ.get('password'))
 
