645aaf6ebb62b3a9a86765aa92618f96a53a9357
InsaneCoder-404 <kemdavid11@gmail.com>
2021-05-02 15:37:28 +0800

    Initial Commit

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ postDescription = "**Guidance on your cultivation techniques, advice on your bot
                   "\n\n#[List of all Weekly Question Threads here](https://www.reddit.com/r/" + target_subreddit + "/wiki/" + target_wiki + ") "
 topReplyText = "Any suggestions for the subreddit? If you have some then you can share your ideas in this comment!."
 submissionURL = "https://www.reddit.com/r/redditdev/comments/6img6p/editing_a_wiki_page_with_praw/"
-delay = 10080
+delay = 604800
 flairID = "ebeec660-73f3-11ea-978e-0e93b6116963"
 wikiContents = "#What is this page?\n**• This page will be the list of weekly threads and many more! So fellow " \
                "cultivators can easily navigate through previous discussions.**\n\n" \
