059e7aba881023fa78989db8c38a2bc6a8c79be3
InsaneCoder-404 <kemdavid11@gmail.com>
2021-05-21 21:27:55 +0800

    Changing of contents

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ target_subreddit = "MartialMemes"
 target_wiki = "index"
 postTitle = "What part of the novel you are reading this week that you find interesting and you would like to share"
 postDescription = "**Share what are your thoughts on what have you find interesting about the novel you have read this " \
-                  "week!\n Example: \"I totally thought this guy will take this development but instead took that " \
-                  "development to his character.\" Of course, please use spoiler tags for spoiler-ey events or names**\n" \ 
+                  "week!\n\nExample: \"I totally thought this guy will take this development but instead took that " \
+                  "development to his character.\"\n\nOf course, please use spoiler tags for spoiler-ey events or names**\n" \ 
                   "\n\n#[List of all Weekly Threads here](https://www.reddit.com/r/" + target_subreddit + "/wiki/" + target_wiki + ") "
 topReplyText = "Any suggestions for the subreddit? If you have some then you can share your ideas in this comment!."
 submissionURL = "https://www.reddit.com/r/redditdev/comments/6img6p/editing_a_wiki_page_with_praw/"
